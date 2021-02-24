78aafd59a91d810740c883c3c3fdedd22a74e576
Mikael Norlén <mickenorlen@gmail.com>
2021-02-24 14:47:52 +0100

    Update cmds

--- utils.sh
+++ utils.sh
@@ -31,7 +31,7 @@ function pullall() { # Pull all updates including submodules
 declare -A commitall_args=(
   ['1']='commit msg'
 ); function commitall() { #
-  git submodule foreach bash -c "orb git has_uncommitted && git add . && git commit -m \"$1\" || echo 'nothing new'"
+  git submodule foreach bash -c "orb git has_uncommitted && git add . && git commit -m \"$1\" || :"
 
   if orb git has_uncommitted; then
     git add .
